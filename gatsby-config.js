8794942226407c2d0f9c28b2b370bdcdb39738cb
camille villard <hello@camillevillard.com>
2019-04-06 17:10:54 +0100

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Samantha Gallay | Avocat`,
+    description: `avocat à Bordeaux`,
+    author: `cdltbisou`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,23 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = ({ graphql, actions }) => {
+  const createWpPosts = new Promise((resolve, reject) => {
+    const query = graphql(`
+      {
+        allWordpressPost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `)
 
-// You can delete this file if you're not using it
+    query.then(result => {
+      console.log(JSON.stringify(result, null, 4))
+      resolve()
+    }) // query.then
+  }) // createWpPosts
+
+  return Promise.all([createWpPosts])
+} // createPages
