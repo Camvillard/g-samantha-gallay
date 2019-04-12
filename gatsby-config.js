37982776d42d10a1f0a8e956c3eaf2c48ce57403
camille villard <hello@camillevillard.com>
2019-04-12 18:32:28 -0400

    built the navbar with WP pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
          "**/posts",
          "**/pages",
          "**/tags",
+         "**/menus",
        ],
       },
      },
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
 // external libraries
 import React from "react";
-// import { graphql } from "gatsby";
+import { graphql, StaticQuery } from "gatsby";
 
 // internal data
 // import Logo from "../images/samanthagallay-logo_blanc.svg";
 
 import '../styles/main.scss';
 
-const links = ["à propos", "expertise", "honoraires", "contact", "nouvelles"]
+const visibleLinks = ["à propos", "expertise", "honoraires", "contact", "nouvelles"];
+let navbarLinks = [];
 
-const MobileMenu = () => {
+const MobileMenu = (props) => {
   return(
     <ul id="main-menu" className="mobile-menu" >
       <li>hamburger menu here</li>
@@ -17,23 +18,75 @@ const MobileMenu = () => {
   )
 }
 
-const DesktopMenu = () => {
+const DesktopMenu = (props) => {
+  // console.log("prout",props)
   return(
     <ul id="main-menu" className="desktop-menu">
-      { links.map( (link, index) => <li key={`${index}`}>{link}</li>) }
+    { props.data.map( link => {
+        return <li key={link.node.id}><a href={`${link.node.slug}`}>{link.node.title}</a></li>
+      })
+    }
     </ul>
   )
 }
 
 class Navbar extends React.Component {
 
-  render() {
-    if (window.innerWidth > 992) {
-      return <DesktopMenu  />
+  checkNavbarLink(link) {
+    if (visibleLinks.includes(link.node.title)) {
+      navbarLinks.push(link)
+      console.log("this is the final navbar links array", navbarLinks)
     }
-    return <MobileMenu />
+  }
+
+  componentWillMount() {
+    console.log("mounting the component", navbarLinks)
+    const data = this.props.data.allWordpressPage.edges
+    data.map( edge => {
+      this.checkNavbarLink(edge)
+      }
+    )
+  }
+
+  // buildNavbarLink(links) {
+  //   links.map( link => {
+  //     return <li key={link.node.id}><a href={`${link.node.slug}`}>{link.node.title}</a></li>
+  //   })
+  // }
+
+  render() {
+    console.log("rendering", navbarLinks)
+    if (window.innerWidth > 992) {
+      return <DesktopMenu data={navbarLinks}/>
+    }
+    return <MobileMenu data={this.props.data}/>
   }
 
 }
 
-export default Navbar;
+// export default Navbar;
+
+
+export default (props) => {
+  return (<StaticQuery
+    query={graphql`
+      query { allWordpressPage {
+          edges {
+            node {
+              id
+              slug
+              title
+            }
+          }
+        }
+      }
+    `
+    }
+
+    render={(data) => {
+      return (
+        <Navbar data={data} />
+      )}
+    }
+  />)
+}
